b30fe5405a558cbd11d6b5e9f567f0943ca6344c
Simon F Nilsson <simon.f.nilsson@me.com>
2026-02-15 12:40:32 +0100

    Altered structure to make more sense, added git

---
+++ git/install.sh
@@ -0,0 +1,28 @@
+# Detect OS
+if [[ -f /etc/arch-release ]]; then
+    OS="arch"
+elif [[ -f /etc/lsb-release ]] || [[ -f /etc/debian_version ]]; then
+    OS="ubuntu"
+else
+    echo "Unsupported OS. Please install zsh manually."
+    exit 1
+fi
+
+# 1. Install zsh if not installed
+if ! command -v git > /dev/null 2>&1; then
+    echo "Installing git..."
+    if [[ "$OS" == "arch" ]]; then
+        sudo pacman -S --noconfirm git
+    elif [[ "$OS" == "ubuntu" ]]; then
+        sudo apt update
+        sudo apt install -y git
+    fi
+else
+    echo "git is already installed."
+fi
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo "Adding gitconfig"
+sudo cp "${SCRIPT_DIR/.gitconfig ~/.gitconfig}"
+echo "Adding gitconfig finished"
--- install.sh
+++ install.sh
@@ -2,7 +2,14 @@ echo "Running installation scripts for dotfiles"
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+echo "Running git install script"
+PATH="$SCRIPT_DIR/git/install.sh"
+chmod +x $PATH
+"./$PATH"
+echo "Git install script finished"
+
 echo "Running zsh install script"
-chmod +x "$SCRIPT_DIR/git/zsh/install.sh"
-./git/zsh/install.sh
+PATH="$SCRIPT_DIR/zsh/install.sh"
+chmod +x "$SCRIPT_DIR/zsh/install.sh"
+"./$PATH"
 echo "Zsh install script finished"
