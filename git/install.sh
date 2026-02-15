2187abf4a602eda1131d9eafd16d4cf5171368e1
Simon F Nilsson <simon.f.nilsson@me.com>
2026-02-15 13:00:07 +0100

    fixed path and shell weirdness

--- git/install.sh
+++ git/install.sh
@@ -1,19 +1,19 @@
 # Detect OS
-if [[ -f /etc/arch-release ]]; then
+if [ -f /etc/arch-release ]; then
     OS="arch"
-elif [[ -f /etc/lsb-release ]] || [[ -f /etc/debian_version ]]; then
+elif [ -f /etc/lsb-release ] || [ -f /etc/debian_version ]; then
     OS="ubuntu"
 else
-    echo "Unsupported OS. Please install zsh manually."
+    echo "Unsupported OS. Please install git manually."
     exit 1
 fi
 
 # 1. Install zsh if not installed
 if ! command -v git > /dev/null 2>&1; then
     echo "Installing git..."
-    if [[ "$OS" == "arch" ]]; then
+    if [ "$OS" == "arch" ]; then
         sudo pacman -S --noconfirm git
-    elif [[ "$OS" == "ubuntu" ]]; then
+    elif [ "$OS" == "ubuntu" ]; then
         sudo apt update
         sudo apt install -y git
     fi
@@ -21,8 +21,8 @@ else
     echo "git is already installed."
 fi
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
 echo "Adding gitconfig"
-sudo cp "${SCRIPT_DIR/.gitconfig ~/.gitconfig}"
+sudo cp "$SCRIPT_DIR/.gitconfig" "$HOME/.gitconfig"
 echo "Adding gitconfig finished"
--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 echo "Running installation scripts for dotfiles"
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
 echo "Running git install script"
-PATH="$SCRIPT_DIR/git/install.sh"
-chmod +x $PATH
-"./$PATH"
+INSTALL_PATH="$SCRIPT_DIR/git/install.sh"
+chmod +x "$INSTALL_PATH"
+"$INSTALL_PATH"
 echo "Git install script finished"
 
 echo "Running zsh install script"
-PATH="$SCRIPT_DIR/zsh/install.sh"
-chmod +x "$SCRIPT_DIR/zsh/install.sh"
-"./$PATH"
+INSTALL_PATH="$SCRIPT_DIR/zsh/install.sh"
+chmod +x "$INSTALL_PATH"
+"$INSTALL_PATH"
 echo "Zsh install script finished"
